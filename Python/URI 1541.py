0d8c8a6d34bd7898ec0e3fce021f6da16ec2f85d
Luiz Gustavo Mesquita <lgustm@github.com>
2021-03-06 23:59:21 -0300

    adicionei novos códigos

--- Python/URI 1541.py
+++ Python/URI 1541.py
@@ -1,7 +1,14 @@
-x1, x2, x3 = input().split(" ")
+import math
 
-area = x1 * x2
-lado = area/x3
-lado = lado**1/2
+while True:
+    entrada= input()
 
-print('{}'.format(lado))
+    if entrada == '0': 
+        break
+
+    x1, x2, x3 = entrada.split()
+
+    area = ((int(x1) * int(x2)) * 100) /int(x3)
+    lado = int(math.sqrt(area))
+
+    print('{}'.format(lado))
