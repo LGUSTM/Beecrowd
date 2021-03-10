fd2a6d97c85533f5f166b6a4052e1fe1e2641cdb
Luiz Gustavo Mesquita <lgustm@github.com>
2021-03-10 11:46:43 -0300

    adicionei novos códigos

--- Python/URI 1847.py
+++ Python/URI 1847.py
@@ -3,26 +3,23 @@ a = int(a)
 b = int(b)
 c = int(c)
 
-if b < a and c > b or c == b:
-    print(":)")
-if b > a and c < b or c == b:
-    print(":(")
-x = b - a
-y = c - b
-if b > a and c > b: 
-    if y < x:
-        print(":(")
-    if y >= x:
+if a>b:
+    if b>c:
+        if (b-c) < (a-b): 
+            print(":)")
+        else:
+            print(":(")
+    else:
         print(":)")
-x = b - c
-y = a - b 
-if b < a and c < b: 
-    if x < y:
-        print(":)")
-    if x >= y:
-        print(":(")
-if a == b:
-    if c > b:
-        print(":)")
+elif b>a:
+    if c>b:
+        if (c-b) < (b-a):
+            print(":(")
+        else: 
+            print(":)")
     else:
         print(":(")
+elif c>b: 
+    print(":)")
+else:
+    print(":(")
---
+++ Python/URI 1864.py
@@ -0,0 +1,5 @@
+quote = "LIFE IS NOT A PROBLEM TO BE SOLVED"
+
+entrada = int(input())
+
+print("{}".format(quote[0:entrada]))
